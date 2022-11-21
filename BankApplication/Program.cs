47a51be687f6328e523a3ef2e107b5677be0692f
lucas-ohlin <lucas.persson.ohlin@hotmail.com>
2022-11-21 11:56:39 +0100

    Add project files.

---
+++ BankApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BankApplication {
+
+    internal class Program {
+
+        private static void Main(string[] args) {
+
+            Console.WriteLine("Hello World!");
+
+        }
+
+    }
+}
